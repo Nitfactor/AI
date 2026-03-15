636f0a10a816e6c4931a5e6264b446a47aa57604
Akshat Singh <nitfactor@gmail.com>
2026-03-15 16:33:31 +0530

    Delete news_ingestion/extracter/article_extracter.py

--- news_ingestion/extracter/article_extracter.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = "0dc5fef2f07148819e7641ba49eb9b1b"
-
-def fetch_news():
-    url = f"https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=0dc5fef2f07148819e7641ba49eb9b1b"
-    response = requests.get(url)
-    data = response.json()
-    return data["articles"]
